c8e659110902db9390ad34215ddebe82cb0849ba
Embu Banjar <48594974+Embu48@users.noreply.github.com>
2019-03-20 21:57:50 +0700

    penomoran search

--- Find Words.cpp
+++ Find Words.cpp
@@ -138,7 +138,7 @@ int diagonal2(char* z){
 }
 
 int main(){
- 	int n;
+ 	int n,(*Find[])(char*)={horizontal,vertikal,diagonal1,diagonal2};
 	char soal[15][15]= {	{'t','g','b','w','w','i','n','t','e','r','w','s','e','s','n'},
 				{'a','a','u','n','t','t','m','m','h','f','o','o','d','n','b'},
 				{'j','l','w','c','q','l','d','z','m','p','m','v','d','m','r'},
@@ -154,8 +154,7 @@ int main(){
 				{'m','o','s','g','z','c','z','e','t','d','b','o','o','t','o'},
 				{'p','d','c','r','z','m','s','n','g','r','d','n','r','p','z'},
 				{'o','h','n','k','z','w','a','t','e','r','j','g','t','r','a'}};
- int (*Find[])(char*)={horizontal,vertikal,diagonal1,diagonal2};
-	
+
 	cout<<"FIND WORDS!\n\n";
 	for(int x=0;x<15;x++){
   		for(int y=0;y<15;y++){
@@ -168,20 +167,20 @@ int main(){
 	char w[n][15];
 	cout<<"\nDaftar kata yang akan dicari : \n";
 	for (int i=0;i<n;i++){
-		cin>>w[i];
+		cout<<i+1<<". ";cin>>w[i];
 	}
 	cout<<endl<<"Hasil : \n";
 	for (int y=0;y<n;y++){
 		f=0;
 		for (int a=0;a<4;a++){
 			if((*Find[a])(w[y]) == 1){
-				cout<<"ada";
+				cout<<y+1<<". ada";
 				f++;
 				break;
 			}
 		}
 		if (f == 0)
-			cout<<"tidak";
+			cout<<y+1<<". tidak";
 		cout<<endl;
 	}
  return 0;
