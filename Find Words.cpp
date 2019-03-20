936acb3ce7d9a72afe6594e72f55f5ca2633791d
Embu Banjar <48594974+Embu48@users.noreply.github.com>
2019-03-20 15:23:14 +0700

    Update Find Words.cpp
    Main file

---
+++ Find Words.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int horizontal(){
+}
+
+int vertikal(){
+}
+
+int diagonal1(){
+}
+
+int diagonal2(){
+}
+
+int main(){
+ int (*Find[])(char*)={horizontal,vertikal,diagonal1,diagonal2};
+ return 0;
+}
